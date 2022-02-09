57e8e6d18f02ea0afc0f809b73e49ce291a3c103
Vincent Pageaud <vincentpageaud@outlook.fr>
2022-02-09 02:21:24 +0100

    Update header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,11 +5,15 @@ import styles from '../../styles/Header.module.css';
 const Header: React.FC = () => {
   return (
     <header className={styles.container} data-scroll-section>
-      <div className={styles.text} data-scroll data-scroll-speed="1">
-        <h1>Vincent Pageaud</h1>
-        <h2>{useTranslation('mainTitle')}</h2>
+      <div className={styles.text}>
+        <div data-scroll data-scroll-speed="1">
+          <h1>Vincent Pageaud</h1>
+        </div>
+        <div data-scroll data-scroll-speed="4">
+          <h2>{useTranslation('mainTitle')}</h2>
+        </div>
       </div>
-      <div className={styles.picture} data-scroll data-scroll-speed="8" />
+      <div className={styles.picture} data-scroll data-scroll-speed="10" />
     </header>
   );
 };
