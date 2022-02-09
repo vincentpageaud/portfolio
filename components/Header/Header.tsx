1af640f638c352427569c0d63839ef4f0d45670e
Vincent Pageaud <vincentpageaud@outlook.fr>
2022-02-09 02:47:54 +0100

    Improve responsive

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,15 +5,19 @@ import styles from '../../styles/Header.module.css';
 const Header: React.FC = () => {
   return (
     <header className={styles.container} data-scroll-section>
-      <div className={styles.text}>
-        <div data-scroll data-scroll-speed="1">
-          <h1>Vincent Pageaud</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.text}>
+          <div data-scroll data-scroll-speed="1">
+            <h1>Vincent Pageaud</h1>
+          </div>
+          <div data-scroll data-scroll-speed="4">
+            <h2>{useTranslation('mainTitle')}</h2>
+          </div>
         </div>
-        <div data-scroll data-scroll-speed="4">
-          <h2>{useTranslation('mainTitle')}</h2>
+        <div className={styles.picture} data-scroll data-scroll-speed="10">
+          <div className={styles.filter} />
         </div>
       </div>
-      <div className={styles.picture} data-scroll data-scroll-speed="10" />
     </header>
   );
 };
