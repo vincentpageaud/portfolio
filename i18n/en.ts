2470e487286647737fb1b02f42c019f9af5418b2
Vincent Pageaud <vincentpageaud@outlook.fr>
2022-02-16 19:12:10 +0100

    Add global var

--- i18n/en.ts
+++ i18n/en.ts
@@ -2,6 +2,7 @@ import { id } from 'i18n';
 
 const en: id = {
   mainTitle: 'Full stack developer on Paris',
+  learnMore: 'Learn more',
 };
 
 export default en;
--- i18n/fr.ts
+++ i18n/fr.ts
@@ -2,6 +2,7 @@ import { id } from 'i18n';
 
 const fr: id = {
   mainTitle: 'Développeur full stack sur Paris',
+  learnMore: 'En savoir plus',
 };
 
 export default fr;
