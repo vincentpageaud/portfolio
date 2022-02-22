99ad8d9fa01c41a9b123053ee6dc2f5513247944
Vincent Pageaud <vincentpageaud@outlook.fr>
2022-02-22 01:23:35 +0100

    Add configuration

---
+++ configurations/portfolio.ts
@@ -0,0 +1,12 @@
+const portfolioConf = {
+  keyservices: {
+    name: 'KeyServices',
+    logoUrl: '/projects/keyservices.png',
+  },
+  thetiptop: {
+    name: 'ThéTipTop',
+    logoUrl: '/projects/thetiptop.png',
+  },
+};
+
+export default portfolioConf;
