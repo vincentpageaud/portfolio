6dd5e802adb703d0c14b09948c406c1ad88dd9a5
Vincent Pageaud <vincentpageaud@outlook.fr>
2022-02-26 19:06:03 +0100

    Add portfolio descriptions

--- configurations/portfolio.ts
+++ configurations/portfolio.ts
@@ -8,28 +8,39 @@ const portfolioConf = {
     description: [
       {
         language: 'EN',
-        contents: '',
+        contents:
+          'On this fictional project, I made a full concierge PWA. With the management of properties, user profiles, and the possibility of contacting his advisor. I created the whole UI/UX, the Front and the Back.',
       },
       {
         language: 'FR',
-        contents: '',
+        contents:
+          'Sur ce projet fictif, j’ai réalisé une PWA de conciergerie complète. Avec la gestion des biens, des profils utilisateurs, et la possibilité de contacter son conseiller. J’ai créé l’ensemble de l’UI/UX, le Front et le Back.',
       },
     ],
+    // links: {
+    //   website: 'http://google.com/',
+    //   git: {
+    //     host: 'GitHub',
+    //     url: 'http://youtube.com/',
+    //   },
+    // },
   },
   thetiptop: {
     name: 'ThéTipTop',
     logoUrl: '/projects/thetiptop.png',
     coverUrl: '/projects/thetiptopcover.png',
-    type: 'Full stack',
+    type: 'Front End',
     tags: ['TypeScript', 'React', 'NextJS', 'Rematch', 'Material UI', 'Axios', 'SwiperJS', 'NodeJS', 'Koa', 'MongoDB'],
     description: [
       {
         language: 'EN',
-        contents: '',
+        contents:
+          'On this studies project, I created a contest-game site. With the possibility of drawing a lot directly, and participating in the final draw, launched from a back office. I created the Front via a UI/UX provided, participated in the creation of the Back routes, and set up the workflow.',
       },
       {
         language: 'FR',
-        contents: '',
+        contents:
+          'Sur ce projet d’études, j’ai réalisé un site de jeu-concours. Avec la possibilité de tirer un lot directement, et de participer au tirage au sort final, lancé depuis un back office. J’ai créé le Front via une UI/UX fournie, participé à la création des routes du Back, et mis en place le workflow.',
       },
     ],
   },
