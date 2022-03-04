44ae1586c01a1e5fcff3e838e89fe4d5e7ae97eb
Vincent Pageaud <vincentpageaud@outlook.fr>
2022-03-04 03:08:59 +0100

    Add scale prop

--- components/Skills/components/AnimatedSphere.tsx
+++ components/Skills/components/AnimatedSphere.tsx
@@ -1,11 +1,15 @@
 import { Sphere, MeshDistortMaterial } from '@react-three/drei';
 
-const AnimatedSphere: React.FC = () => {
+interface Props {
+  scale: number;
+}
+
+const AnimatedSphere: React.FC<Props> = ({ scale }) => {
   return (
     <>
       {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
       {/* @ts-ignore */}
-      <Sphere visible args={[1, 100, 200]} scale={1}>
+      <Sphere visible args={[1, 100, 200]} scale={scale}>
         {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
         {/* @ts-ignore */}
         <MeshDistortMaterial color="#8352FD" attach="material" distort={0.3} speed={1.5} roughness={0} />
