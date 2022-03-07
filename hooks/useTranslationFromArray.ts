928b274e759383617726c8f92383410ae3d3cd3e
Vincent Pageaud <vincentpageaud@outlook.fr>
2022-03-07 20:26:05 +0100

    Update hook import from model

--- hooks/useTranslationFromArray.ts
+++ hooks/useTranslationFromArray.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
-import { Description } from 'models/PortfolioModel';
+import { Description } from 'models/GenericModel';
 
 const useTranslationFromArray = (array: Description[]): Description => {
   const [localizedArray, setLocalizedArray] = React.useState<Description>({});
