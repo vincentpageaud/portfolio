8f897d053531ac52b5917bab64e7024ab1a8b4b2
Vincent Pageaud <vincentpageaud@outlook.fr>
2022-03-08 17:33:07 +0100

    Temp datas

---
+++ configurations/skills.ts
@@ -0,0 +1,31 @@
+const skillsConf = {
+  summary: [
+    {
+      language: 'EN',
+      contents: 'EN desc',
+    },
+    {
+      language: 'FR',
+      contents: [
+        "Développeur Freelance passionné, en sortie d'école, j'ai pu acquérir de nombreuses compétences à partir de mes projets d'études, mes capacités autodidactes, ainsi que de mes deux ans dans une entreprise orientée vers le divertissement à bord (Trains, avions, Ferry, etc.).",
+        "J'ai donc pu travailler avec de nombreux outils afin de réaliser des cartes interactives, lecteurs vidéo et musique, liseuses de presse, etc.",
+      ],
+    },
+  ],
+  frontEnd: ['React', 'NextJS'],
+  backEnd: ['NodeJS', 'Express'],
+  cvUrl: [
+    {
+      language: 'EN',
+      title: 'Download the CV (in French)',
+      url: '/CV.pdf',
+    },
+    {
+      language: 'FR',
+      title: 'Télécharger le CV',
+      url: '/CV.pdf',
+    },
+  ],
+};
+
+export default skillsConf;
