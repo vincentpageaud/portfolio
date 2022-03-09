95a78f6c48cb25daa49d664246c615a7730c7498
Vincent Pageaud <vincentpageaud@outlook.fr>
2022-03-09 17:32:29 +0100

    Add waves to footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,20 @@
-import styles from '../../styles/Footer.module.css';
+// import dynamic from 'next/dynamic';
+
+import AnimatedWaves from './components/AnimatedWaves';
+
+import styles from 'styles/Footer.module.css';
+
+// const Map = dynamic(() => import('./components/Map'), {
+//   ssr: false,
+// });
 
 const Footer: React.FC = () => {
-  return <footer className={styles.container} data-scroll-section></footer>;
+  return (
+    <footer className={styles.container} data-scroll-section>
+      <AnimatedWaves />
+      {/* <Map /> */}
+    </footer>
+  );
 };
 
 export default Footer;
---
+++ components/Footer/components/AnimatedWaves.tsx
@@ -0,0 +1,34 @@
+import styles from 'styles/Footer.module.css';
+
+const AnimatedWaves: React.FC = () => {
+  return (
+    <div className={styles.wavesContainer}>
+      <svg
+        className={styles.wavesWrapper}
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 24 150 28"
+        preserveAspectRatio="none"
+      >
+        <defs>
+          <path
+            id="gentle-wave"
+            d="M-160 44c30 0 
+                58-18 88-18s
+                58 18 88 18 
+                58-18 88-18 
+                58 18 88 18
+                v44h-352z"
+          />
+        </defs>
+        <g className={styles.waves}>
+          <use xlinkHref="#gentle-wave" x="50" y="0" fill="#21606a" />
+          <use xlinkHref="#gentle-wave" x="50" y="3" fill="var(--secondary-color)" />
+          <use xlinkHref="#gentle-wave" x="50" y="6" fill="#21386a" />
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+export default AnimatedWaves;
