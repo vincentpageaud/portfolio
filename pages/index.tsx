68c1ddec7791c44738259d427b464573b063f2e7
Vincent Pageaud <vincentpageaud@outlook.fr>
2022-03-11 13:09:42 +0100

    Remove leaflet css import

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,12 +22,6 @@ const Home: NextPage = () => {
         <title>Vincent Pageaud | {useTranslation('mainTitle')}</title>
         <meta name="description" content={contents} />
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-          crossOrigin=""
-        />
       </Head>
       <Header />
       <Portfolio />
