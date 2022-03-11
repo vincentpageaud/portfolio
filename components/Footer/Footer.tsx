443283825b3b432b1625db5e91aa42b662e76c5a
Vincent Pageaud <vincentpageaud@outlook.fr>
2022-03-11 12:52:21 +0100

    Remove leaflet

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,18 +1,11 @@
-// import dynamic from 'next/dynamic';
-
 import AnimatedWaves from './components/AnimatedWaves';
 
 import styles from 'styles/Footer.module.css';
-
-// const Map = dynamic(() => import('./components/Map'), {
-//   ssr: false,
-// });
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.container} data-scroll-section>
       <AnimatedWaves />
-      {/* <Map /> */}
     </footer>
   );
 };
