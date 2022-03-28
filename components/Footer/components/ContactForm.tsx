b622f15a3566c0b36bd7474a03b114b411af7a6d
Vincent Pageaud <vincentpageaud@outlook.fr>
2022-03-28 23:54:14 +0200

    Add regex

--- components/Footer/components/ContactForm.tsx
+++ components/Footer/components/ContactForm.tsx
@@ -14,12 +14,35 @@ const ContactForm: React.FC = () => {
     message: '',
   });
   const [isSent, setIsSent] = useState(false);
+  const [formError, setFormError] = useState({
+    fullName: false,
+    email: false,
+    message: false,
+  });
+  const nameReg = /^[a-z ,.'-]+$/i;
+  const emailReg =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   const send = useTranslation('send');
   const sending = useTranslation('sending');
   const sent = useTranslation('sent');
 
   const { actions, loading: isSending, error, success } = useContactForm();
+
+  const handleBlur: Handler = (e) => {
+    switch (e.target.id) {
+      case 'fullName':
+        setFormError({ ...formError, fullName: !nameReg.test(e.target.value) });
+        break;
+      case 'email':
+        setFormError({ ...formError, email: !emailReg.test(e.target.value) });
+        break;
+      case 'message':
+        setFormError({ ...formError, message: e.target.value.length === 0 });
+        console.log(e.target.value.length);
+        break;
+    }
+  };
 
   const handleChange: Handler = (e) => {
     setContactForm({ ...contactForm, [e.target.id]: e.target.value });
@@ -42,21 +65,41 @@ const ContactForm: React.FC = () => {
     }
   }, [success]);
 
+  const isInputError =
+    formError.fullName ||
+    formError.email ||
+    formError.message ||
+    contactForm.fullName.length === 0 ||
+    contactForm.email.length === 0 ||
+    contactForm.message.length === 0;
+
   return (
     <form className={styles.formContainer}>
       <h1>{useTranslation('contactForm')}</h1>
-      <label htmlFor="fullName">{useTranslation('fullName')}</label>
-      <input type="text" id="fullName" value={contactForm.fullName} onChange={handleChange} />
-      <label htmlFor="email">{useTranslation('mailAddress')}</label>
-      <input type="email" id="email" value={contactForm.email} onChange={handleChange} />
-      <label htmlFor="message">Message</label>
-      <textarea name="" id="message" rows={10} value={contactForm.message} onChange={handleChange}></textarea>
+      <label className={formError.fullName ? styles.error : ''} htmlFor="fullName">
+        {useTranslation('fullName')}
+      </label>
+      <input type="text" id="fullName" value={contactForm.fullName} onChange={handleChange} onBlur={handleBlur} />
+      <label className={formError.email ? styles.error : ''} htmlFor="email">
+        {useTranslation('mailAddress')}
+      </label>
+      <input type="email" id="email" value={contactForm.email} onChange={handleChange} onBlur={handleBlur} />
+      <label className={formError.message ? styles.error : ''} htmlFor="message">
+        Message
+      </label>
+      <textarea
+        name=""
+        id="message"
+        rows={10}
+        value={contactForm.message}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
       <div className={styles.error} style={{ opacity: error ? 1 : 0 }}>
         {useTranslation('networkError')}
       </div>
       <div
-        // eslint-disable-next-line no-constant-condition
-        className={`${styles.button} ${isSent || isSending || true ? styles.disabled : ''}`}
+        className={`${styles.button} ${isSent || isSending || isInputError ? styles.disabled : ''}`}
         onClick={handleSubmit}
         role="button"
         onKeyDown={() => []}
