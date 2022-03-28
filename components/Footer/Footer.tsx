7c4f488c3e46f03ce9187fa834bc8fa3b5cbeff8
Vincent Pageaud <vincentpageaud@outlook.fr>
2022-03-28 20:42:26 +0200

    Add navigation to footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,3 +1,5 @@
+import { useLocomotiveScroll } from 'react-locomotive-scroll';
+
 import useTranslation from 'hooks/useTranslation';
 
 import ContactForm from './components/ContactForm';
@@ -5,8 +7,10 @@ import ContactForm from './components/ContactForm';
 import styles from 'styles/Footer.module.css';
 
 const Footer: React.FC = () => {
+  const { scroll } = useLocomotiveScroll();
+
   return (
-    <footer className={styles.container} data-scroll-section>
+    <footer id="footer" className={styles.container} data-scroll-section>
       <div className={styles.chromeFix} />
       <h1 className={styles.mainTitle}>{useTranslation('contactMe')}</h1>
       <div className={styles.wrapper}>
@@ -32,6 +36,47 @@ const Footer: React.FC = () => {
               Malt
             </a>
           </p>
+          <nav>
+            <h1>Navigation</h1>
+            <div>
+              <a
+                className={styles.button}
+                onClick={() => scroll.scrollTo('#top')}
+                onKeyDown={() => null}
+                role="button"
+                tabIndex={0}
+              >
+                {useTranslation('home')}
+              </a>
+              <a
+                className={styles.button}
+                onClick={() => scroll.scrollTo('#portfolio')}
+                onKeyDown={() => null}
+                role="button"
+                tabIndex={0}
+              >
+                {useTranslation('Portfolio')}
+              </a>
+              <a
+                className={styles.button}
+                onClick={() => scroll.scrollTo('#skills')}
+                onKeyDown={() => null}
+                role="button"
+                tabIndex={0}
+              >
+                {useTranslation('skills')}
+              </a>
+              <a
+                className={styles.button}
+                onClick={() => scroll.scrollTo('#footer')}
+                onKeyDown={() => null}
+                role="button"
+                tabIndex={0}
+              >
+                {useTranslation('contactMe')}
+              </a>
+            </div>
+          </nav>
         </div>
       </div>
     </footer>
