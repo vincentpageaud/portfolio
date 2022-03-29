210221c853f6920ddfbe8db80e43d219acadf46b
Vincent Pageaud <vincentpageaud@outlook.fr>
2022-03-29 18:35:12 +0200

    Remove useless maj

--- configurations/skills.ts
+++ configurations/skills.ts
@@ -3,14 +3,14 @@ const skillsConf = {
     {
       language: 'EN',
       contents: [
-        'Passionate freelance developer, just graduated, I was able to acquire many skills from my study projects, my self-taught abilities, as well as my two years in a company geared towards on board entertainment (Trains, Planes, Ferries, and so on.).',
+        'Passionate freelance developer, just graduated, I was able to acquire many skills from my study projects, my self-taught abilities, as well as my two years in a company geared towards on board entertainment (trains, planes, ferries, and so on.).',
         'So I was able to work with many tools to create interactive maps, video and music players, press readers, and so on.',
       ],
     },
     {
       language: 'FR',
       contents: [
-        "Développeur Freelance passionné, en sortie d'école, j'ai pu acquérir de nombreuses compétences à partir de mes projets d'études, mes capacités autodidactes, ainsi que de mes deux ans dans une entreprise orientée vers le divertissement à bord (Trains, avions, Ferries, etc.).",
+        "Développeur Freelance passionné, en sortie d'école, j'ai pu acquérir de nombreuses compétences à partir de mes projets d'études, mes capacités autodidactes, ainsi que de mes deux ans dans une entreprise orientée vers le divertissement à bord (trains, avions, ferries, etc.).",
         "J'ai donc pu travailler avec de nombreux outils afin de réaliser des cartes interactives, lecteurs vidéo et musique, liseuses de presse, etc.",
       ],
     },
