cd47e0f47cb45445520537dd5a81e0003608c1de
Vincent Pageaud <vincentpageaud@outlook.fr>
2023-12-17 01:24:41 +0100

    Add contentful to next/image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   i18n: {
-    locales: ["en", "fr"],
-    defaultLocale: "en"
-  }
-}
+    locales: ['en', 'fr'],
+    defaultLocale: 'en',
+  },
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
+};
