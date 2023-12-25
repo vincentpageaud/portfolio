e463c79c65df15a890649402731596b940a2d38b
Vincent Pageaud <vincentpageaud@outlook.fr>
2023-12-25 23:56:56 +0100

    Add fetchFooters

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,33 @@
 import { NextPage, NextPageContext } from 'next';
 import Head from 'next/head';
+import { documentToPlainTextString } from '@contentful/rich-text-plain-text-renderer';
 
 import Header from '@components/Header';
 import Portfolio from '@components/Portfolio';
 import Skills from '@components/Skills';
 import Footer from '@components/Footer';
-import { fetchHeaders, fetchPortfolios, fetchSkills } from '@services/contentful';
+import { fetchHeaders, fetchPortfolios, fetchSkills, fetchFooters } from '@services/contentful';
 import { Headers as HeadersModel } from '@models/HeadersModel';
 import { Portfolios as PortfoliosModel } from '@models/PortfoliosModel';
 import { Skills as SkillsModel } from '@models/SkillsModel';
-
-import useTranslationFromArray from '@hooks/useTranslationFromArray';
-
-import skillsConf from '@configs/skills';
+import { Footers as FootersModel } from '@models/FootersModel';
 
 interface Props {
-  headers: HeadersModel;
-  portfolios: PortfoliosModel;
-  skills: SkillsModel;
+  headers?: HeadersModel;
+  portfolios?: PortfoliosModel;
+  skills?: SkillsModel;
+  footers?: FootersModel;
 }
 
-const Home: NextPage<Props> = ({ headers, portfolios, skills }) => {
-  let { contents } = useTranslationFromArray(skillsConf.summary);
-
-  if (Array.isArray(contents)) contents = contents.join(' ');
+const Home: NextPage<Props> = ({ headers, portfolios, skills, footers }) => {
+  const metaContent =
+    skills?.items && skills.items[0].fields?.summary && documentToPlainTextString(skills.items[0].fields.summary);
 
   return (
     <>
       <Head>
-        <title>Vincent Pageaud | {headers.items[0]?.fields.title}</title>
-        <meta name="description" content={contents} />
+        <title>Vincent Pageaud | {headers?.items && headers.items[0]?.fields?.title}</title>
+        <meta name="description" content={metaContent} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header data={headers} />
@@ -37,15 +35,27 @@ const Home: NextPage<Props> = ({ headers, portfolios, skills }) => {
         <Portfolio data={portfolios} />
         <Skills data={skills} />
       </main>
-      <Footer />
+      <Footer data={footers} />
     </>
   );
 };
 
 export const getStaticProps = async (ctx: NextPageContext) => {
-  const result = await Promise.all([fetchHeaders(ctx.locale), fetchPortfolios(ctx.locale), fetchSkills(ctx.locale)]);
+  const result = await Promise.all([
+    fetchHeaders(ctx.locale),
+    fetchPortfolios(ctx.locale),
+    fetchSkills(ctx.locale),
+    fetchFooters(ctx.locale),
+  ]);
 
-  return { props: { headers: result[0], portfolios: result[1], skills: result[2] } };
+  return {
+    props: {
+      headers: result[0],
+      portfolios: result[1],
+      skills: result[2],
+      footers: result[3],
+    },
+  };
 };
 
 export default Home;
