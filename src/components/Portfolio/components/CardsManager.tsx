1824d3cdc93b78279d7eeaf1c705f0ed50c95d57
Vincent Pageaud <vincentpageaud@outlook.fr>
2024-05-05 02:17:47 +0200

    Change card direction

--- src/components/Portfolio/components/CardsManager.tsx
+++ src/components/Portfolio/components/CardsManager.tsx
@@ -4,18 +4,17 @@ import Card from './Card';
 
 interface Props {
   data?: Project[];
-};
+}
 
 const CardsManager: React.FC<Props> = ({ data }) => {
-
   return (
     <>
       {data?.map((card, index) => {
         const isPair = index - (1 % 2) === 0 ? true : false;
         return isPair ? (
-          <Card key={card.sys.id} direction="right" variant="secondary" datas={card} />
+          <Card key={card.sys.id} direction="left" variant="secondary" datas={card} />
         ) : (
-          <Card key={card.sys.id} direction="left" datas={card} />
+          <Card key={card.sys.id} direction="right" datas={card} />
         );
       })}
     </>
